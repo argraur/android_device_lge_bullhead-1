89bc2540228b2ef2be3c36323c7cdfeccab0c8f6
Sid-Sun <sidharth.soni525@gmail.com>
2017-03-15 12:25:33 +0530

    Xenonified Bullhead tree
    Signed-off-by: Sid-Sun <sidharth.soni525@gmail.com>

--- xenonhd.mk
+++ xenonhd.mk
@@ -3,19 +3,33 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_bullhead
+PRODUCT_NAME := xenonhd_bullhead
+PRODUCT_DEVICE := bullhead
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5X
 TARGET_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
+#Don't ship SuperSU
+WITH_SUPERSU := false
+
+#Ship snap cam && cam 2
+PRODUCT_PACKAGES += \
+    Camera2 \
+    Snap
+
+#Maintainer Info
+PRODUCT_PROPERTY_OVERRIDES += ro.xenonhd.maintainer=Sid
+
+# Use the latest approved GMS identifiers unless running a signed build
+ifneq ($(SIGN_BUILD),true)
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=bullhead \
     BUILD_FINGERPRINT="google/bullhead/bullhead:7.1.1/N4F26T/3687331:user/release-keys" \
     PRIVATE_BUILD_DESC="bullhead-user 7.1.1 N4F26T 3687331 release-keys"
+endif
